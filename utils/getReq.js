cc3dc08c050a4673c33a8ecac0ac4a3cf34529df
Josiah Leon <sjosiahleon@gmail.com>
2025-02-17 09:50:55 -0800

    updated getPopTitles to get the pop from the last month

--- utils/getReq.js
+++ utils/getReq.js
@@ -1,9 +1,16 @@
 
 
 
-// get top 10 popular titles
+// get top 10 popular titles over the last month
 export const getPopTitles = async () => {
-    let res = await fetch('https://api.mangadex.org/manga?includes[]=cover_art&includes[]=artist&includes[]=author&order[followedCount]=desc&contentRating[]=safe&contentRating[]=suggestive&hasAvailableChapters=true&createdAtSince=2025-01-18T08%3A00%3A00')
+    const lastMonth = new Date();
+    lastMonth.setHours(0, 0, 0, 0);
+    lastMonth.setDate(lastMonth.getDate());
+    lastMonth.setMonth(lastMonth.getMonth() - 1);
+
+    const midnightISO = lastMonth.toISOString().split('.')[0];
+    
+    let res = await fetch(`https://api.mangadex.org/manga?includes[]=cover_art&includes[]=artist&includes[]=author&order[followedCount]=desc&contentRating[]=safe&contentRating[]=suggestive&hasAvailableChapters=true&createdAtSince=${encodeURIComponent(midnightISO)}`)
     let data = await res.json()
     console.log(data.data);
     return data.data
