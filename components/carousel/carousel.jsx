46e4d3566b54ecc706277da3ca06b024f83558b4
Josiah Leon <sjosiahleon@gmail.com>
2025-02-17 21:53:32 -0800

    updated carousel to show title and desc, prettied it up

--- components/carousel/carousel.jsx
+++ components/carousel/carousel.jsx
@@ -1,10 +1,11 @@
 'use client'
 import Image from "next/image";
 import { getCoverUrl, getDesc, getENTitle } from "@/utils/mangaManipulation";
-import {useState} from 'react';
+import { useState } from 'react';
+import Link from "next/link";
 
 
-const Carousel = (props) => {
+const Carousel = ({mangas}) => {
     let [curPage, setCurPage] = useState(0);
 
     // add a setinterval to move the idx along every 40 to 60 secs or so?
@@ -12,15 +13,15 @@ const Carousel = (props) => {
     const genPages = () => {
         let pages = [];
 
-        for (let idx = 0; idx < props.mangas.length; idx++) {
+        for (let idx = 0; idx < mangas.length; idx++) {
             pages.push(
             <li
                 key={idx}
-                className={`mx-1 transition-all hover:animate-pulse hover:scale-105 hover:opacity-100 ${ idx === curPage ? '!opacity-100 animate-pulse scale-105' : 'opacity-30'}`}
+                className={`mx-1 h-fill transition-all hover:animate-pulse hover:scale-105 hover:opacity-100 ${ idx === curPage ? '!opacity-100 animate-pulse scale-105' : 'opacity-30'}`}
                 onClick={(e) => {setCurPage((idx))}}
             >
                 <Image
-                    src={getCoverUrl(props.mangas[idx])}
+                    src={getCoverUrl(mangas[idx])}
                     width={720}
                     height={1280}
                     alt="manga cover"
@@ -38,16 +39,21 @@ const Carousel = (props) => {
 
     return (
         <>
-            <div className="relative"><h2 className="fixed top-16 left-40">Up & Coming</h2></div>
+            {/* <div className="relative"><h2 className="fixed top-16 left-40">Up & Coming</h2></div> */}
             <ol className="w-full h-1/4 flex overflow-hidden">
                 
                 {
                     genPages()
                 }    
             </ol>
-            {/* <div className="fixed"><div className="relative w-screen h-56 bg-gradient-to-b from-transparent to-gray-500"></div></div> */}
-            <h3 className="font-sigmarOne rounded bg-zinc-700 sticky -mt-10 mb-2 p-2 text-xl text-rose-700">{getENTitle(props.mangas[curPage])}</h3>
-            <div className="w-2/3 h-28 bg-gray-700 bg-opacity-40 rounded p-2 overflow-x-auto">{getDesc(props.mangas[curPage])}</div>
+            <h1 className="font-sigmarOne -mt-14 text-2xl z-10 bg-gray-800 rounded-t px-2">Up and Coming</h1>
+            <article className="w-3/6 p-2  bg-gray-800 z-10 rounded-xl flex flex-col">
+                <div className="flex justify-between mb-1 border-b border-rose-500">
+                    <Link href="#" className="flex items-center" style={{width: "84%", height: "3rem"}}><h3 className="font-sigmarOne text-rose-500 text-ellipsis" title={getENTitle(mangas[curPage])}>{getENTitle(mangas[curPage])}</h3></Link>
+                    <Link href="#" className="font-robotoCondensed w-30 h-8 text-center underline p-1 hover:text-rose-500">Go To Manga</Link>
+                </div>
+                <section className="font-robotoCondensed">{getDesc(mangas[curPage])}</section>
+            </article>
         </>
     );
 };
