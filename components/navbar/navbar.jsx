55dd781ae111bf6fe54f99c1e282461d1704c5dd
Josiah Leon <sjosiahleon@gmail.com>
2025-02-17 22:11:00 -0800

    added opaque bar if scrolled on navbar

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,22 +1,46 @@
+'use client'
 import Image from 'next/image';
 import Link from 'next/link';
 
 import profileIcon from '@/public/icons/account_circle.svg';
 import menuButton from '@/public/icons/menu.svg';
+import { useEffect, useState, useCallback } from 'react';
+
 
 const Navbar = () => {
+    const [showBg, setShowBg] = useState(false);
+
+    const onScroll = useCallback(e => {
+        const {scrollY} = window;
+        
+        if (scrollY > 50) {
+            setShowBg(true);
+        } else {
+            setShowBg(false);
+        }
+    });
+
+    useEffect(() => {
+        window.addEventListener('scroll', onScroll, {passive: true});
+
+        return () => {
+            window.removeEventListener('scroll', onScroll, {passive: true});
+        }
+    }, []);
 
     return (
-        <header className="fixed top-2 flex justify-between w-2/3 font-sigmarOne z-10">
+        <>
+        <div className={`w-screen z-20 h-16 fixed top-0 ${showBg ? 'bg-gray-800' : ''}`}/>
+        <header className="fixed top-2 flex justify-between w-2/3 font-sigmarOne z-50">
             <div className='flex items-center'>
-                <div className='rounded-md bg-gray-700  mr-3'>
+                <div className='rounded-md bg-gray-700 mr-1'>
                     <Image
                         src={menuButton}
                         alt="button to open sidebar"
-                        className='w-11 '
+                        className='w-11'
                     />
                 </div>
-                <Link href='/'><h1 className='font-bold text-2xl text-rose-700'>MangaPulse</h1></Link>
+                <Link href='/'><h1 className='font-bold bg-opacity-90 p-1 text-2xl text-rose-500 drop-shadow-xl'>MangaPulse</h1></Link>
                 
             </div>
     
@@ -36,6 +60,7 @@ const Navbar = () => {
                 </div>
             </div>
         </header>
+        </>
     )
 }
 
