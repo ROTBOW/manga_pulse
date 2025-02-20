f560ec53436c2ad212cd25425f252ff38be210df
Josiah Leon <sjosiahleon@gmail.com>
2025-02-19 16:24:16 -0800

    fixing lastest chap css issues

--- components/homePageComps/latestChapters/latestChapters.jsx
+++ components/homePageComps/latestChapters/latestChapters.jsx
@@ -1,4 +1,4 @@
-import { getChapterScansGroup } from "@/utils/mangaManipulation";
+import { getChapterScansGroup, timeSince } from "@/utils/mangaManipulation";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,19 +7,19 @@ const LateChapItem = ({chapter}) => {
 
     return (
         <li className="flex p-1 h-20 w-full mb-2 font-robotoCondensed items-center">
-            <Link href="#" className="w-14 h-20 mr-2 bg-rose-700 rounded">
+            <Link href="#" className="min-w-14 min-h-20 w-14 h-20 mr-2 bg-rose-700 rounded">
                 <Image 
                     src={chapter.cover_art} 
                     width="56" 
                     height="80"
-                    alt="mangaThumbnail"
-                    className="w-full h-full object-cover object-center rounded"
+                    alt={`${chapter.title}'s Thumbnail`}
+                    className="w-14 h-full object-cover object-center rounded"
                     style={{}}
                 />
             </Link>
-            <div className="" style={{width: '14rem'}}>
-            <Link href="#"><h4 className="truncate w-full" title={chapter.title}>{chapter.title}</h4></Link>
-                <p className="text-sm truncate">{
+            <div className="w-full">
+                <Link href="#"><h4 className="truncate w-4/5" title={chapter.title}>{chapter.title}</h4></Link>
+                <p className="text-sm truncate w-4/5">{
                 `
                 ${chapter.attributes.volume ? "Vol. " + chapter.attributes.volume +' ' : ''}
                 ${chapter.attributes.chapter ? "Ch. " + chapter.attributes.chapter +' ' : ''}
@@ -27,8 +27,9 @@ const LateChapItem = ({chapter}) => {
                 `}</p>
                 {/* need to add a link to the chapter directly from above */}
 
-                <div>
-                    <div className="text-rose-500 text-sm truncate w-40 mt-3">SG: {getChapterScansGroup(chapter)}</div>
+                <div className="flex flex-between w-full text-sm items-end">
+                    <div className="text-rose-500 truncate w-1/2 mt-3">SG: {getChapterScansGroup(chapter)}</div>
+                    <div className="w-1/2 text-end text-nowrap">{timeSince(chapter.attributes.updatedAt)}</div>
                 </div>
             </div>
         </li>
@@ -53,24 +54,25 @@ const LatestChapters = ({ chapters }) => {
         return olItems;
     }
 
-    const olClass = 'bg-gray-800 p-2 mx-2 rounded-md w-1/3';
+    const olClass = 'bg-gray-800 p-2 mx-2 rounded-md w-1/3 inline';
+    const olStyle = {minWidth: '18.75rem'};
     return (
         <div className="flex flex-col items-center mt-14 w-4/5">
-            <h2 className="w-full font-sigmarOne text-rose-500 text-xl">Latest Chapters</h2>
+            <h2 className="w-full font-sigmarOne text-rose-500 text-2xl">Latest Chapters</h2>
             <section className="mt-2 w-full flex justify-between">
-                <ol className={`${olClass}`}>
+                <ol className={olClass} style={olStyle}>
                     {
                         getXtoYChapters(0, 5)
                     }
                 </ol>
 
-                <ol className={`${olClass}`}>
+                <ol className={olClass} style={olStyle}>
                     {
                         getXtoYChapters(6, 11)
                     }
                 </ol>
 
-                <ol className={`${olClass}`}>
+                <ol className={olClass} style={olStyle}>
                     {
                         getXtoYChapters(12, 17)
                     }
